925d9ace72907f1aa35c03749fac9f73fb4b0482
Matvei Kotenev <kotenev.matvei@gmail.com>
2025-01-10 22:32:32 +0100

    remove build

--- ha3/code_blatt03/build/CMakeFiles/Blatt3.dir/structures/CSRMatrix.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/Blatt3.dir/structures/CSRMatrix.cpp.o: \
- /home/matvei/wr/ha3/code_blatt03/structures/CSRMatrix.cpp \
- /usr/include/stdc-predef.h
--- ha4/code_blatt04/build/CMakeFiles/Blatt4.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for Blatt4.
-# This may be replaced when dependencies are built.
